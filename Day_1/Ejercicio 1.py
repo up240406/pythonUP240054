3f38a1eb8aa5f109e596f5a074fd725ae82837a7
UP240054 <up240054@alumnos.upa.edu.mx>
2025-02-25 22:38:03 -0600

    python

---
+++ Day_1/Ejercicio 1.py
@@ -0,0 +1,38 @@
+#Exercise 1
+
+#Addition
+print (3+4)
+
+#Subtraction
+print (3-4)
+
+#Multiplication 
+print (3*4)
+
+#Modulus
+print (3%4)
+
+#Division 
+print (3/4)
+
+#Exponential
+print (3**4)
+
+#Floor Divition 
+print (3//4)
+
+#Name
+name = 'Gael'
+print (name)
+
+#Family name 
+family_name = 'Tujillo Morones'
+print (family_name)
+
+#Country 
+country = 'Mexico'
+print (country)
+
+#Python 
+phrase = 'I am enjoying 30 days of python'
+print (phrase)
---
+++ Day_1/Ejercicio 2.py
@@ -0,0 +1,10 @@
+#Ejercicio 2
+
+print (type(10)) 
+print (type(9.8))
+print (type(3.14))
+print (type(4 - 4j))
+print (type(['Asabeneh', 'Python', 'Finland']))
+print (type('Gael'))
+print (type(['Kike', 'Luz', 'Enrique']))
+print (type('Mexico'))
